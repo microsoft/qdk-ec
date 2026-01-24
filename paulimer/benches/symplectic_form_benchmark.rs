b37f79beb29f6258387e846989090b35d4d2cfd5
Adam Paetznick <adampaetznick@gmail.com>
2026-01-23 16:12:24 -0800

    Prepare for public. (#16)
    * Update changelog.
    
    * Remove flamegraph dependency.

--- paulimer/benches/symplectic_form_benchmark.rs
+++ paulimer/benches/symplectic_form_benchmark.rs
@@ -77,16 +77,7 @@ pub fn symplectic_form_benchmark(criterion: &mut Criterion) {
     group.finish();
 }
 
-#[cfg(not(unix))]
 criterion_group!(benches, symplectic_form_benchmark);
-
-#[cfg(unix)]
-criterion_group! {
-    name = benches;
-    config = Criterion::default().with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
-    targets = symplectic_form_benchmark
-}
-
 criterion_main!(benches);
 
 /// Generate a full X/Z basis for the given number of qubits
@@ -159,81 +150,4 @@ fn generate_random_product_paulis(count: usize) -> Vec<SparsePauli> {
     }
 
     products
-}
-
-//
-//
-//
-//
-//
-// Copyright 2020 TiKV Project Authors. Licensed under Apache-2.0.
-#[cfg(unix)]
-use pprof::flamegraph::Options as FlamegraphOptions;
-
-#[cfg(unix)]
-use criterion::profiler::Profiler;
-#[cfg(unix)]
-use pprof::ProfilerGuard;
-
-#[cfg(unix)]
-use std::fs::File;
-#[cfg(unix)]
-use std::os::raw::c_int;
-#[cfg(unix)]
-use std::path::Path;
-
-#[cfg(unix)]
-#[allow(clippy::large_enum_variant)]
-pub enum Output<'a> {
-    Flamegraph(Option<FlamegraphOptions<'a>>),
-}
-#[cfg(unix)]
-pub struct PProfProfiler<'a, 'b> {
-    frequency: c_int,
-    output: Output<'b>,
-    active_profiler: Option<ProfilerGuard<'a>>,
-}
-#[cfg(unix)]
-impl<'b> PProfProfiler<'_, 'b> {
-    #[must_use]
-    pub fn new(frequency: c_int, output: Output<'b>) -> Self {
-        Self {
-            frequency,
-            output,
-            active_profiler: None,
-        }
-    }
-}
-#[cfg(unix)]
-impl Profiler for PProfProfiler<'_, '_> {
-    fn start_profiling(&mut self, _benchmark_id: &str, _benchmark_dir: &Path) {
-        self.active_profiler = Some(ProfilerGuard::new(self.frequency).unwrap());
-    }
-
-    fn stop_profiling(&mut self, _benchmark_id: &str, benchmark_dir: &Path) {
-        std::fs::create_dir_all(benchmark_dir).unwrap();
-
-        let filename = match self.output {
-            Output::Flamegraph(_) => "flamegraph.svg",
-        };
-        let output_path = benchmark_dir.join(filename);
-        let output_file = File::create(&output_path)
-            .unwrap_or_else(|_| panic!("File system error while creating {}", output_path.display()));
-
-        if let Some(profiler) = self.active_profiler.take() {
-            match &mut self.output {
-                Output::Flamegraph(options) => {
-                    let default_options = &mut FlamegraphOptions::default();
-                    let options = options.as_mut().unwrap_or(default_options);
-
-                    profiler
-                        .report()
-                        .build()
-                        .unwrap()
-                        .flamegraph_with_options(output_file, options)
-                        .expect("Error while writing flamegraph");
-                }
-            }
-        }
-    }
 }
