b9638d8ece925bdc89ca99b9e05560e420d0f1f4
Adam Paetznick <adpaetzn@microsoft.com>
2026-06-06 02:18:34 +0000

    Remove errata.

--- errata/src/errata/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Placeholder package reserving the ``errata`` name on PyPI.
-
-The real implementation is not yet published. See
-https://github.com/microsoft/qdk-ec for details.
-"""
-
-__version__ = "0.0.0"
